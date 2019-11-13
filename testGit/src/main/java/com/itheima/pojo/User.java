0a98f00a28968623273b23625de776dba67877f6
xie20001103 <56616928+xie20001103@users.noreply.github.com>
2019-11-13 20:19:07 +0800

    提交了User

---
+++ testGit/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package com.itheima.pojo;
+
+/**
+ * com.itheima.pojo
+ * XieZhangQuan
+ * 2019-11-13  20:14
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
